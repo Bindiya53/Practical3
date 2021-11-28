12f4802a53c2b0c8c1bd18c45d44f87846386d9d
Bindiya53 <91846798+Bindiya53@users.noreply.github.com>
2021-11-28 19:07:09 +0530

    Add files via upload

---
+++ quizque.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      question : "The wors Yoga is derived from a sanskri word meaning what?",
+      answer : "Union",
+      options: ["Spirit","Peace","Union","Posture"]
+    },
+    {
+      question : "What is not one priciple taught in yoga ?",
+      answer : "Strength",
+      options: ["Proper exercise","Relaxation","Strength","Posture"]
+    },
+    {
+      question : "Which country does yoga originate from?",
+      answer : "India",
+      options: ["Bangladesh","Thailand","India","USA"]
+    },
+    {
+      question : "What does hatha mean?",
+      answer : "Sun and Moon",
+      options: ["Fire and Water","Light and Dark","Sun and Moon","Light and Heavy"]
+    },
+    {
+      question : "What is Chakra?",
+      answer : "Energy Centre",
+      options: ["Prayer Point","Karma","Energy Centre","Health"]
+    },
+  ]
+  
+  module.exports = questions;
